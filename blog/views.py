1922fba683f3eed558f04275c6a6b698daea33e0
bernardopiane <bernardopiane@users.noreply.github.com>
2021-11-26 00:41:49 -0300

    post details fetching from db

--- blog/views.py
+++ blog/views.py
@@ -1,60 +1,26 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 # Create your views here.
 
 # Dictionary with dummy data for posts including slug, title, created_date and author
-all_posts = [
-    {
-        "title": "My first post",
-        "slug": "my-first-post",
-        "date": date(2020, 1, 1),
-        "author": "John Doe",
-        "image": "1.png",
-        "excert": "My first blog post",
-        "content": """Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-          explicabo sequi est officia modi, magni beatae quo iusto totam cum
-          alias laboriosam, maxime suscipit excepturi impedit qui dolorum esse
-          ipsum"""
-    },
-    {
-        "title": "My second post",
-        "slug": "my-second-post",
-        "date": date(2020, 1, 1),
-        "author": "Jane Doe",
-        "image": "2.jpg",
-        "excert": "My second blog post",
-        "content": """Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-          explicabo sequi est officia modi, magni beatae quo iusto totam cum
-          alias laboriosam, maxime suscipit excepturi impedit qui dolorum esse
-          ipsum"""
-    },
-    {
-        "title": "My third post",
-        "slug": "my-third-post",
-        "date": date(2020, 1, 1),
-        "author": "Jack Doe",
-        "image": "3.png",
-        "excert": "My third blog post",
-        "content": """Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-          explicabo sequi est officia modi, magni beatae quo iusto totam cum
-          alias laboriosam, maxime suscipit excepturi impedit qui dolorum esse
-          ipsum"""
-    },
-]
+all_posts = []
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=lambda k: k['date'], reverse=True)
-    latest_posts = sorted_posts[-3:]
+    # Fetch the 3 latest posts
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {'posts': latest_posts})
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {'posts': all_posts})
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, 'blog/post-detail.html', {'post': identified_post})
+    post_by_slug = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post-detail.html', {'post': post_by_slug})
